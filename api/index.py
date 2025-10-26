12763387fe532b70cb39d9fa6d874fd7f6eb89db
Dhinesh-Fedor <dhineshkumar2341@gmail.com>
2025-10-26 19:08:28 +0530

    fix issues

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ import requests
 from datetime import datetime
 import sys
 from dotenv import load_dotenv 
-from mangum import Mangum
+
 
 
 
@@ -173,13 +173,8 @@ def get_status():
     
     
 # Vercel runs the app, so this is not needed for deployment
-#if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+     app.run(host="0.0.0.0", port=5000, debug=True)
 
 
-# Create a serverless handler
-handler = Mangum(app)
 
-# Vercel expects a callable named `main`
-def main(req=None, res=None):
-     return handler(req, res)
